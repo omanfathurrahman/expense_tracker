334b5ccbf68aa8a8e3586081b766a47319cbf3c7
Fathur Rahman <oman.fathur.rahman.nur@gmail.com>
2023-07-25 17:20:32 +0700

    Setting an AppBar with a Title & Actions

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: ExpensesState());
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: const ExpensesState(),
+    );
   }
 }
